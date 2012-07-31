7de7bd28bc7d09b1627d361c35456f863849d7f0
Erik Pukinskis <erik@snowedin.net>
2012-07-30 21:18:07 -0700

    Respond to mouse down and up

--- assets/js/draw_area.js
+++ assets/js/draw_area.js
@@ -1,17 +1,29 @@
+function msg(message) {
+  document.getElementById("message").innerHTML = message
+}
+
 var DrawArea = function(canvas) {
   this.ctx = canvas.getContext("2d");  
   this.ctx.fillStyle = "rgb(200,0,0)";  
+  this.down = false;
 
   this.down = function() {
+    msg("down");
+    this.isDown = true;
   }
 
   this.up = function() {
+    msg("up");
+    this.isDown = false;
   }
 
   this.move = function() {
-    var x = 300 * Math.random();
-    var y = 300 * Math.random();
+    msg("move");
+    if (this.isDown == true) {
+      var x = 300 * Math.random();
+      var y = 300 * Math.random();
 
-    this.ctx.fillRect (x, y, x+5, y+5);  
+      this.ctx.fillRect (x, y, x+5, y+5);  
+    }
   }
 }

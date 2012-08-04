34e9f3ef8008235d059b0ac1798058c02c737295
Erik Pukinskis <erik@snowedin.net>
2012-08-04 11:07:00 -0700

    Add touch events

--- assets/js/draw_area.js
+++ assets/js/draw_area.js
@@ -1,33 +1,57 @@
+function preventBehavior(e) {
+    //alert('touchmove');
+    e.preventDefault(); 
+};
+document.addEventListener("touchmove", preventBehavior, false);
+
 function msg(message) {
   document.getElementById("message").innerHTML = message
 }
 
 var DrawArea = function(canvas) {
-  canvas.width = document.width;
-  canvas.height = document.height;
+  canvas.width = 400;
+  canvas.height = 400;
   canvas.ctx = canvas.getContext("2d");  
   canvas.ctx.fillStyle = "rgb(200,0,0)";  
   canvas.isDown = false;
+  msg("ready");
 
-  canvas.onmousedown = function(e) {
+  canvas.markstart = function(p) {
     msg("down");
-    this.start = e;
+    this.start = p;
     this.isDown = true;
   }
 
-  canvas.onmouseup = function() {
+  canvas.markend = function(p) {
     msg("up");
     this.isDown = false;
   }
 
-  canvas.onmousemove = function(e) {
-    msg(e.clientX);
+  canvas.markmove = function(p) {
+    msg(p.x + "," + p.y);
     if (this.isDown == true) {
       this.ctx.beginPath();  
-      this.ctx.moveTo(this.start.clientX, this.start.clientY);
-      this.ctx.lineTo(e.clientX, e.clientY);
+      this.ctx.moveTo(this.start.x, this.start.y);
+      this.ctx.lineTo(p.x, p.y);
       this.ctx.stroke();
-      this.start = e;
+      this.start = p;
     }
   }
+
+  canvas.connectEvents = function(touch, mouse, mark) {
+    canvas.addEventListener(mouse, function(e) {
+      this[mark]({x: e.clientX, y: e.clientY});
+    });
+
+    canvas.addEventListener(touch, function(e) {
+      e.preventDefault();
+      if (mark != 'touchend') {
+        this[mark]({x: e.touches[0].pageX, y: e.touches[0].pageY});
+      }
+    });
+  }
+
+  canvas.connectEvents('touchstart', 'mousedown', 'markstart');
+  canvas.connectEvents('touchend', 'mouseup', 'markend');
+  canvas.connectEvents('touchmove', 'mousemove', 'markmove');
 }
